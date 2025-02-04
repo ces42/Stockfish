2314e95939993e0da713cc4c68338752b9252bc9
Carlos Esparza <ca2010es@gmail.com>
2025-02-03 23:45:31 -0800

    maybe ~.2% slowdown (but not clear if significant)

--- src/position.cpp
+++ src/position.cpp
@@ -351,6 +351,7 @@ void Position::set_state() const {
         Square s  = pop_lsb(b);
         Piece  pc = piece_on(s);
         st->key ^= Zobrist::psq[pc][s];
+        ++st->pieceCount;
 
         if (type_of(pc) == PAWN)
             st->pawnKey ^= Zobrist::psq[pc][s];
@@ -712,6 +713,7 @@ void Position::do_move(Move                      m,
     // Used by NNUE
     st->accumulatorBig.computed[WHITE]     = st->accumulatorBig.computed[BLACK] =
       st->accumulatorSmall.computed[WHITE] = st->accumulatorSmall.computed[BLACK] = false;
+    st->commonParentPos = false;
 
     auto& dp     = st->dirtyPiece;
     dp.dirty_num = 1;
@@ -742,6 +744,7 @@ void Position::do_move(Move                      m,
 
     if (captured)
     {
+        --st->pieceCount;
         Square capsq = to;
 
         // If the captured piece is a pawn, update pawn hash key, otherwise
@@ -1030,6 +1033,7 @@ void Position::do_null_move(StateInfo& newSt, const TranspositionTable& tt) {
     st->dirtyPiece.piece[0]                = NO_PIECE;  // Avoid checks in UpdateAccumulator()
     st->accumulatorBig.computed[WHITE]     = st->accumulatorBig.computed[BLACK] =
       st->accumulatorSmall.computed[WHITE] = st->accumulatorSmall.computed[BLACK] = false;
+    st->commonParentPos = false;
 
     st->pliesFromNull = 0;
 
