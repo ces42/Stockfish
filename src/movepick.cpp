cd0a4fdc7f8138a8b32b165e27171f972e5eff04
Carlos Esparza <ca2010es@gmail.com>
2025-05-18 22:14:03 -0700

    optimize performance
    bench: 2326210

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -129,9 +129,10 @@ void MovePicker::score() {
     Color us = pos.side_to_move();
 
     [[maybe_unused]] Bitboard threatenedPieces, threatByLesser[QUEEN + 1];
-    Color us = pos.side_to_move();
+    [[maybe_unused]] Color us;
     if constexpr (Type == QUIETS)
     {
+        us = pos.side_to_move();
         threatByLesser[KNIGHT] = threatByLesser[BISHOP] = pos.attacks_by<PAWN>(~us);
         threatByLesser[ROOK] =
           pos.attacks_by<KNIGHT>(~us) | pos.attacks_by<BISHOP>(~us) | threatByLesser[KNIGHT];
@@ -175,10 +176,10 @@ void MovePicker::score() {
             {
                 if (pt <= QUEEN) {
                     static constexpr int bonus[QUEEN + 1] = {0, 0, 144, 144, 256, 517};
-                    int v = (threatByLesser[pt] & to ? -95 : 100 * bool(threatByLesser[pt] & from));
+                    int v = threatByLesser[pt] & to ? -95 : 100 * bool(threatByLesser[pt] & from);
                     m.value += bonus[pt] * v;
                 } else
-                    m.value += 8192 * popcount(pos.blockers_for_king(us));
+                    m.value += 4096 * popcount(pos.blockers_for_king(us));
             }
 
             if (ply < LOW_PLY_HISTORY_SIZE)
