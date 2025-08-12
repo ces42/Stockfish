2eba8e18f50eeb07ee51fb92ed18c1647a6b83ed
Carlos Esparza <ca2010es@gmail.com>
2025-08-12 17:54:57 +0200

    values from previous tune
    bench: 3777178

--- src/search.cpp
+++ src/search.cpp
@@ -545,19 +545,20 @@ void Search::Worker::undo_null_move(Position& pos) { pos.undo_null_move(); }
 
 
 int knightPSQT[RANK_NB][int(FILE_NB) / 2] = {
-    { -175, -92, -74, -73 },
-    { -77, -41, -27, -15 },
-    { -61, -17, 6, 12 },
-    { -35, 8, 40, 49 },
-    { -34, 13, 44, 51 },
-    { -9, 22, 58, 53 },
-    { -67, -27, 4, 37 },
-    { -201, -83, -56, -26 }
+    { -183, -76, -66, -73, },
+    { -60, -54, -43, -15, } ,
+    { -50, -3,  -11, 13,  } ,
+    { -24, 1,   33,  47,  } ,
+    { -29, 8,   27,  42,  } ,
+    { -2,  14,  68,  48,  } ,
+    { -62, -17, 8,   43,  } ,
+    { -186, -93, -51, -22, },
 };
 
 int mh0 = 64;
+
 TUNE(mh0);
-int psqtW = 320;
+int psqtW = 349;
 TUNE(psqtW);
 TUNE(SetRange(-300, 200), knightPSQT);
 
@@ -609,10 +610,10 @@ void Search::Worker::clear() {
     refreshTable.clear(networks[numaAccessToken]);
 }
 
-int offs[KING + 1] = {630, 630, 630, 630, 630, 630, 630};
-int mh_mult = 935;
-int bonus_mult = 224;
-int mh_weight = 71;
+int offs[KING + 1] = {651, 597, 629, 652, 626, 677, 613};
+int mh_mult = 883;
+int bonus_mult = 242;
+int mh_weight = 70;
 TUNE(offs, mh_mult, bonus_mult, mh_weight);
 
 
