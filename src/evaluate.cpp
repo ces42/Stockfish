d947410d03acdf3728bb42a29edd5998b0c92976
Carlos Esparza <ca2010es@gmail.com>
2025-08-18 00:16:21 +0200

    higher base
    bench: 2269284

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -48,7 +48,7 @@ int Eval::simple_eval(const Position& pos) {
 
 bool Eval::use_smallnet(const Position& pos, bool last_big) {
     // return std::abs(simple_eval(pos)) > 962;
-    return std::abs(simple_eval(pos)) > 830 + 150 * last_big;
+    return std::abs(simple_eval(pos)) > 900 + 150 * last_big;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
