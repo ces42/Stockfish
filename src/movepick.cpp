696a3649e9c19fcffb1b0781d86593350bfef0db
Carlos Esparza <ca2010es@gmail.com>
2026-08-06 00:21:33 +0200

    replace see by cheap check

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -237,7 +237,9 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += ((pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
+            m.value += (
+                (pos.check_squares(pt) & to) && !(to & pos.threats_by<ALL_PIECES>())
+            ) * 16384;
 
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
